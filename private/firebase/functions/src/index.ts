d627b8a2e24883528d50d4849de6495cc46af58c
Aaron Lockhart <aaronlockhartdev@gmail.com>
2021-03-26 22:44:18 -0400

    Cleaned up eslint style

--- private/firebase/functions/src/index.ts
+++ private/firebase/functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
+admin.initializeApp()
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -10,24 +11,25 @@ import * as admin from "firebase-admin";
 //   response.send("Hello from Firebase!");
 // });
 
-function getFormattedDate(){
-    let d = new Date();
+function getFormattedDate() {
+  const d = new Date();
     
-    let date_formatted = d.getFullYear() + "-" + ('0' + (d.getMonth() + 1)).slice(-2) + "-" + ('0' + d.getDate()).slice(-2) + " " + ('0' + d.getHours()).slice(-2) + ":" + ('0' + d.getMinutes()).slice(-2) + ":" + ('0' + d.getSeconds()).slice(-2);
+  const dateFormatted = d.getFullYear() + "-" + ("0" + (d.getMonth() + 1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2) + " " 
+                        + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + ":" + ("0" + d.getSeconds()).slice(-2);
 
-    return date_formatted;
+  return dateFormatted;
 }
 
 exports.createUser = functions.auth.user().onCreate(async(user) => {
-    const newUser = {
-        email: user.email,
-        createDate: admin.firestore.FieldValue.serverTimestamp(),
-        username: user.email?.substring(0, user.email?.indexOf('@'))
-    }
+  const newUser = {
+    email: user.email,
+    createDate: admin.firestore.FieldValue.serverTimestamp(),
+    username: user.email?.substring(0, user.email?.indexOf("@")),
+  }
 
-    const userRef = admin.firestore().collection('users');
+  const userRef = admin.firestore().collection("users");
 
-    await userRef.doc(String(user.uid)).set(newUser);
+  await userRef.doc(String(user.uid)).set(newUser);
 
-    console.log(`${getFormattedDate()} - [* createUser *] :: Created user ${newUser.username}`);
+  console.log(`${getFormattedDate()} :: Created user ${newUser.username}`);
 })
