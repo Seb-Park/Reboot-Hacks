3b60b7687331d887be62f2d00603a9487481af78
Sebastian Park <SebPark03@gmail.com>
2021-03-27 09:51:56 -0400

    Fix chrome extension to realize whether we're in session or not. Also make it so there's a nice list and naughty list of websites.
    The nice list and naughty list are just local right now.

--- public/chrome-extension/injections/inject.js
+++ public/chrome-extension/injections/inject.js
@@ -1,59 +1,132 @@
-var fonts = document.createElement("link");
-fonts.rel = "stylesheet";
-fonts.href = "https://fonts.googleapis.com/css2?family=Lexend:wght@100;300;400;500;600&family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;1,300;1,400;1,600&display=swap";
+function displayOverlay() {
+    chrome.storage.local.get("inSession", function (result) {
+        if (result.inSession) {
+            chrome.storage.local.get("goodSites", function (goodSitesResult) {
+                siteIsOkay = false;
+                for (let i = 0; i < goodSitesResult.goodSites.length; i++) {
+                    var website = goodSitesResult.goodSites[i];
+                    console.log(website);
+                    if (window.location.href.includes(website)) {
+                        siteIsOkay = true;
+                        break;
+                    }                    
+                }
+                if (!siteIsOkay) {
+                    var fonts = document.createElement("link");
+                    fonts.rel = "stylesheet";
+                    fonts.href = "https://fonts.googleapis.com/css2?family=Lexend:wght@100;300;400;500;600&family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;1,300;1,400;1,600&display=swap";
 
-document.head.appendChild(fonts);
+                    document.head.appendChild(fonts);
 
-var overlay = document.createElement("div");
-overlay.className = "procrastination-overlay";
+                    var overlay = document.createElement("div");
+                    overlay.className = "procrastination-overlay";
 
-var procrastinationContent = document.createElement("div");
-procrastinationContent.className = "procrastination-content";
+                    var procrastinationContent = document.createElement("div");
+                    procrastinationContent.className = "procrastination-content";
 
-var oopsText = document.createElement("div");
-oopsText.className = "oops-text";
-oopsText.innerHTML = "Oops!";
+                    var oopsText = document.createElement("div");
+                    oopsText.className = "oops-text";
+                    oopsText.innerHTML = "Oops!";
 
-procrastinationContent.appendChild(oopsText);
+                    procrastinationContent.appendChild(oopsText);
 
-var explanation = document.createElement("div");
-explanation.className = "procrastination-explanation";
-explanation.innerHTML = "Looks like this website isn't your homework for right now!"
+                    var explanation = document.createElement("div");
+                    explanation.className = "procrastination-explanation";
+                    explanation.innerHTML = "Looks like this website isn't your homework for right now!"
 
-procrastinationContent.appendChild(explanation)
+                    procrastinationContent.appendChild(explanation)
 
-var buttonOptions = document.createElement("div");
-buttonOptions.className = "procrastination-button-options";
+                    var buttonOptions = document.createElement("div");
+                    buttonOptions.className = "procrastination-button-options";
 
-var button1 = document.createElement("div");
-button1.className = "procrastination-button procrastination-button-1";
-button1.innerHTML = "Yes, it is.";
-button1.addEventListener("click", function(){
-    overlay.remove();
-})
+                    var button1 = document.createElement("div");
+                    button1.className = "procrastination-button procrastination-button-1";
+                    button1.innerHTML = "Yes, it is.";
+                    button1.addEventListener("click", function () {
+                        var simpleUrl = window.location.href.replace("https://","").replace("http://","").replace("www.","").split("/")[0]
+                        addWebsiteToNiceList(simpleUrl)
+                        overlay.remove();
+                    })
 
-var button2 = document.createElement("div");
-button2.className = "procrastination-button procrastination-button-2";
-button2.innerHTML = "Sorry!";
+                    var button2 = document.createElement("div");
+                    button2.className = "procrastination-button procrastination-button-2";
+                    button2.innerHTML = "Sorry!";
 
-button2.addEventListener("click", function(){
-    window.location.href = "https://google.com";
-})
+                    button2.addEventListener("click", function () {
+                        window.location.href = "https://google.com";
+                    })
 
-buttonOptions.appendChild(button1);
-buttonOptions.appendChild(button2);
+                    buttonOptions.appendChild(button1);
+                    buttonOptions.appendChild(button2);
 
-procrastinationContent.appendChild(buttonOptions)
+                    procrastinationContent.appendChild(buttonOptions)
 
-overlay.appendChild(procrastinationContent);
+                    overlay.appendChild(procrastinationContent);
 
-document.body.appendChild(overlay);
+                    document.body.appendChild(overlay);
+                }
 
-// chrome.webRequest.onBeforeRequest.addListener(
-// 	function(details) {
-// 		console.log("blocking:", details.url);
-// 		return {cancel: true };
-// 	},
-// 	{urls: ["*://*.youtube.com/"]},
-// 	["blocking"]
-// );
+                // chrome.webRequest.onBeforeRequest.addListener(
+                // 	function(details) {
+                // 		console.log("blocking:", details.url);
+                // 		return {cancel: true };
+                // 	},
+                // 	{urls: ["*://*.youtube.com/"]},
+                // 	["blocking"]
+                // );
+            });
+        }
+    });
+}
+
+function addWebsiteToNiceList(niceUrl) {
+    chrome.storage.local.get("goodSites", function (goodSitesResult) {
+        var newList = [];
+        if (goodSitesResult.goodSites) {
+            var newList = goodSitesResult.goodSites
+        }
+        // goodSitesResult.goodSites.forEach(website => {
+
+        // });
+        newList.push(niceUrl)
+        chrome.storage.local.set({ goodSites: newList }, function () {
+            console.log('In session is set to ' + newList);
+        });
+    });
+}
+
+displayOverlay();
+
+// chrome.storage.onChanged.addListener(function(changes, namespace) {
+//     for (var key in changes) {
+//       var storageChange = changes[key];
+//       console.log('Storage key "%s" in namespace "%s" changed. ' +
+//                   'Old value was "%s", new value is "%s".',
+//                   key,
+//                   namespace,
+//                   storageChange.oldValue,
+//                   storageChange.newValue);
+//     }
+//   });
+
+// chrome.storage.onChanged.addListener(function(changes, namespace) {
+//     for (var key in changes) {
+//       var storageChange = changes[key];
+//       if(key === "inSession"){
+//           if(storageChange.newValue){
+//             overlay.remove();
+//           }
+//           else{
+//             document.body.appendChild(overlay);
+//           }
+//       }
+//     }
+//   });
+
+// function updateSyncChromeVariable(key, val) {
+//     chrome.storage.sync.set({ key: val }, function () {
+//         console.log('Value is set to ' + val);
+//     });
+// }
+
+// updateSyncChromeVariable("test", false);
--- public/chrome-extension/popup.js
+++ public/chrome-extension/popup.js
@@ -1,30 +1,43 @@
 var placeholderId = "9aU7nSLWOPa0c26Tm1d5dMIbyf32";
+var isInSession = false;
 
 const navigateToPage = async (path) => {
     var xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-    //   console.log(this.responseXML.title);
+    xhr.onload = function () {
+        //   console.log(this.responseXML.title);
         var htmlAsString = new XMLSerializer().serializeToString(this.responseXML);
         document.getElementById("content").innerHTML = htmlAsString;
         var script = document.createElement('script');
-        script.setAttribute('src', path+".js");
+        script.setAttribute('src', path + ".js");
         script.setAttribute('type', 'text/javascript');
         document.body.appendChild(script);
     }
-    xhr.open("GET", path+".html");
+    xhr.open("GET", path + ".html");
     xhr.responseType = "document";
     xhr.send();
 }
 
-//
-//ACTUALLY MAKE THIS CONTINGENT UPON WHETHER THE SESSION IS 
-//ENTERED OR NOT FROM "CHECK SESSION" ENDPOINT
-//
-// navigateToPage("pages/home");
-navigateToPage("pages/page2");
+// function checkSession(){
+//     var url = 'https://us-central1-skedjul-3f13c.cloudfunctions.net/checkSession';
+//     fetch(url, {
+//         method: 'POST',
+//         body: JSON.stringify({
+//             "uid": placeholderId
+//         }),
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     }).then((res) => {
+//         responseJson = res.json();
+//         console.log(responseJson)
+//     }).catch(() => {
+//         alert("Session doc not found. Setting in session to false.")
+//         return {result: false};
+//     });
+// }
 
-const enterSession = async () => {
-    var url = 'https://us-central1-skedjul-3f13c.cloudfunctions.net/enterSession';
+const checkSession = async () => {
+    var url = 'https://us-central1-skedjul-3f13c.cloudfunctions.net/checkSession';
     const response = await fetch(url, {
         method: 'POST',
         body: JSON.stringify({
@@ -38,13 +51,49 @@ const enterSession = async () => {
     return response.json();
 }
 
-function enterSessionWithNavigation(){
+navigateToPage("pages/loading");
+
+checkSession().then((response) => {
+    updateInSessionLocalVariable(response.result);
+    if (response.result) {
+        navigateToPage("pages/page2");
+    }
+    else {
+        navigateToPage("pages/home");
+    }
+}).catch(() => {
+    navigateToPage("pages/home");
+})
+
+//
+//ACTUALLY MAKE THIS CONTINGENT UPON WHETHER THE SESSION IS 
+//ENTERED OR NOT FROM "CHECK SESSION" ENDPOINT
+//
+// navigateToPage("pages/home");
+
+const enterSession = async () => {
+    var url = 'https://us-central1-skedjul-3f13c.cloudfunctions.net/enterSession';
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+            "uid": placeholderId
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    updateInSessionLocalVariable(true);
+    console.log(url);
+    return response.json();
+}
+
+function enterSessionWithNavigation() {
     navigateToPage("pages/loading");
     enterSession().then((result) => {
-        if(result.result==="success"){
+        if (result.result === "success") {
             navigateToPage("pages/page2")
         }
-    }).catch(()=>{
+    }).catch(() => {
         navigateToPage("pages/home")
     })
 }
@@ -60,17 +109,64 @@ const exitSession = async () => {
             "Content-Type": "application/json"
         }
     });
+    updateInSessionLocalVariable(false);
     console.log(url);
     return response.json();
 }
 
-function exitSessionWithNavigation(){
+function exitSessionWithNavigation() {
     navigateToPage("pages/loading");
     exitSession().then((result) => {
-        if(result.result==="success"){
+        if (result.result === "success") {
             navigateToPage("pages/home")
         }
-    }).catch(()=>{
+    }).catch(() => {
         navigateToPage("pages/page2")
     })
 }
+
+// function updateSyncChromeVariable(key, val) {
+//     chrome.storage.sync.set({ key: val }, function () {
+//         console.log('Value is set to ' + val);
+//     });
+// }
+
+// function updateLocalChromeVariable(key, val) {
+//     chrome.storage.local.set({ key : val }, function () {
+//         console.log('Value is set to ' + val);
+//     });
+// }
+
+function updateInSessionLocalVariable(val) {
+    chrome.storage.local.set({ inSession: val }, function () {
+        console.log('In session is set to ' + val);
+    });
+}
+
+function addWebsiteToNiceList(niceUrl) {
+    chrome.storage.local.get("goodSites", function (goodSitesResult) {
+        var newList = [];
+        if (goodSitesResult.goodSites) {
+            var newList = goodSitesResult.goodSites
+        }
+        // goodSitesResult.goodSites.forEach(website => {
+
+        // });
+        newList.push(niceUrl)
+        chrome.storage.local.set({ goodSites: newList }, function () {
+            console.log('In session is set to ' + newList);
+        });
+    });
+}
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (var key in changes) {
+        var storageChange = changes[key];
+        console.log('Storage key "%s" in namespace "%s" changed. ' +
+            'Old value was "%s", new value is "%s".',
+            key,
+            namespace,
+            storageChange.oldValue,
+            storageChange.newValue);
+    }
+});
