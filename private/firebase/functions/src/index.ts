4de959a216abc1c3694a1c78b535e2a4bc8a0ef3
Sebastian Park <SebPark03@gmail.com>
2021-03-27 01:03:51 -0400

    Add the check session firebase function.

--- private/firebase/functions/src/index.ts
+++ private/firebase/functions/src/index.ts
@@ -13,14 +13,14 @@ admin.initializeApp()
 
 function getFormattedDate() {
   const d = new Date();
-    
-  const dateFormatted = d.getFullYear() + "-" + ("0" + (d.getMonth() + 1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2) + " " 
-                        + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + ":" + ("0" + d.getSeconds()).slice(-2);
+
+  const dateFormatted = d.getFullYear() + "-" + ("0" + (d.getMonth() + 1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2) + " "
+    + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + ":" + ("0" + d.getSeconds()).slice(-2);
 
   return dateFormatted;
 }
 
-exports.createUser = functions.auth.user().onCreate(async(user) => {
+exports.createUser = functions.auth.user().onCreate(async (user) => {
   const newUser = {
     email: user.email,
     createDate: admin.firestore.FieldValue.serverTimestamp(),
@@ -33,4 +33,24 @@ exports.createUser = functions.auth.user().onCreate(async(user) => {
   await userRef.doc(String(user.uid)).set(newUser);
 
   console.log(`${getFormattedDate()} :: Created user ${newUser.username}`);
+})
+
+export const checkSession = functions.https.onRequest(async (req: any, res: any) => {
+  const userRef = admin.firestore().collection('users').doc(req.body.uid);
+  // .where('tags', 'array-contains', req.body.q)
+  // .where("event_time", ">=", new Date())
+  // .orderBy("event_time");
+
+  const doc = await userRef.get();
+  if (!doc.exists) {
+    res.status(200).json({
+      result: false,
+      error: "Doc does not exist."
+    })
+  } else {
+    res.status(200).json({
+      result: doc.get("inSession"),
+      error: "none"
+    })
+  }
 })
