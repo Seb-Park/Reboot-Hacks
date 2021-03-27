f6abff8c092709adcbd8f2a1bd8941935ae32991
Sebastian Park <SebPark03@gmail.com>
2021-03-27 02:27:12 -0400

    Fix http request thingy.

--- public/chrome-extension/popup.js
+++ public/chrome-extension/popup.js
@@ -1,20 +1,22 @@
 const enterSession = async () => {
-    const response = await fetch('https://us-central1-skedjul-3f13c.cloudfunctions.net/enterSession', {
-      method: 'POST',
-      body: {
-          uid: "9aU7nSLWOPa0c26Tm1d5dMIbyf32"
-      },
-      headers: {
-        'Content-Type': 'application/json'
-      }
+    var url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+    url = 'https://us-central1-skedjul-3f13c.cloudfunctions.net/enterSession';
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+            "uid": "9aU7nSLWOPa0c26Tm1d5dMIbyf32"
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
     });
-    console.log(response.json())
+    console.log(url);
     return response.json();
-  }
+}
 
 enterSessionButton = document.getElementById("enter-session-button");
-enterSessionButton.addEventListener("click", function(){
-    enterSession().then((data)=> {
-        alert(data);
+enterSessionButton.addEventListener("click", function () {
+    enterSession().then((result) => {
+        alert(result.result);
     })
 });
